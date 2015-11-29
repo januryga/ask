1d801ef99018519908bbdbb0b44bc883e49be3f8
Sawic <sawic24@gmail.com>
2015-11-29 19:56:56 +0100

    Update get_reply.py

--- get_reply.py
+++ get_reply.py
@@ -1,80 +1,50 @@
 # -*- coding: cp1250 -*-
 """ Reply generating component """
 
-def get_reply(user_message,phone):
-	"""
-	Tries to interpret the input from user_message. If the input
-	is a valid command, it calls the right app from apps/ with the
-	interpreted parameters, and returns its output. (string) If the
-	input isn't valid, returns a nice error message. (string)
-	"""
+def grind_input(input):
+    app=input.split(' ')[0]
+    parameters=input.partition(' ')[2]
+    parameters=parameters.split(',')
+    for index,parameter in enumerate(parameters):
+        if parameters[index]=='':
+            parameters.pop(index)
+    return app,parameters
 
-	import apps.twitch
-	import apps.google_maps
-	import apps.google
-	import apps.wikipedia
-	input=user_message
-	input=input.lower()
-	
-	input_list=[None,None,None,None,None]
-	
-	i=0
-	for word in input.split(','):
-	    input_list[i]=word
-	    i=i+1
-	    if i>4:
-		break
-	
-	program=input_list[0]
-	parameter1=input_list[1]
-	parameter2=input_list[2]
-	parameter3=input_list[3]
-	parameter4=input_list[4]
-	
-	"""
-	return ("Requested program: " + program)
-	return ("Parameters: ")
-	
-	for word in input_list[1:]:
-	    if word:
-		return (word + " ")
-	"""
-	if not parameter1 and " " in program:
-	    return ("ERROR: No parameters found for \"" + program.split(' ')[0] +"\".\nPlease separate expressions with \",\".")
-	elif not program:
-	    return ("ERROR: Empty request")
-	elif program=="maps" or program=="google maps":
-	    if not parameter1 or not parameter2 or not parameter3:
-		return ("ERROR: To few parameters")
-	    elif parameter4:
-		return ("ERROR: To many parameters")
-	    else:
-		return (google_maps.find_path_city_a_b(parameter1,parameter2,parameter3))
-	elif program=="twitch":
-	    if not parameter1:
-		return ("ERROR: To few parameters")
-	    elif parameter2 or parameter3:
-		return ("ERROR: To many parameters")
-	    else:
-		return (twitch.get_online_status(parameter1))
-	elif program=="wiki" or program=="wikipedia":
-	    if not parameter1:
-		return ("ERROR: To few parameters")
-	    elif parameter2:
-		return ("ERROR: To many parameters")
-	    else:
-		return (wikipedia.TU WPISZ NAZWE FUKNCJI, KTORA ZWRACA ODPOWIEDZ(parameter1))
+def get_reply(user_message):
+    """
+    Tries to interpret the input from user_message. If the input
+    is a valid command, it calls the right app from apps/ with the
+    interpreted parameters, and returns its output. (string) If the
+    input isn't valid, returns a nice error message. (string)
+    """
 
-	elif program=="google":
-	    if not parameter1:
-		return ("ERROR: To few parameters")
-	    elif parameter2:
-		return ("ERROR: To many parameters")
-	    else:
-		return (google.get_google_search(parameter1))
-	else:
-	    return ("ERROR: \"" + program + "\" - No such service provided")
-	return reply_message
+    #import Apps.Google as Google
+    import Apps.Google_maps as Google_maps
+    #import Apps.Wikipedia as Wikipedia
+    import Apps.Twitch as Twitch
+    import re
+
+    user_message=user_message.lower()
+    app,parameters=grind_input(user_message)
+
+    apps={
+    'twitch':Twitch.get_online_status,
+    #'google':Google.search,
+    #'wiki':Wikipedia.search
+    'maps':Google_maps.find_path_city_a_b
+    };
+
+    try:
+        return (apps[app](*parameters))
+    except TypeError as error:
+        if re.findall(r'\d+',error.args[0])[0]>re.findall(r'\d+',error.args[0])[1]:
+            return ("ERROR: To few arguments given.")
+        else:
+            return ("ERROR: To many arguments given.")
+    except Twitch.WrongUsernameError:
+        return ("ERROR: Wrong Username")
+    except:
+        return "Congrats, You found an unhandable error! Now You can try inserting a correct output (format: \"[service] [parameter1],[parameter2]...\")"
 
 # About the error message: It should make clear what the error actually is.
 # The error cases I could think of:
