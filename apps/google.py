76128724166e7974deb7a7933e05a4b7432b217d
adamis96 <przywartyadam@gmail.com>
2015-12-02 23:10:31 +0100

    create launch.py

--- apps/google.py
+++ apps/google.py
@@ -2,17 +2,20 @@ from search import GoogleSearch, SearchError
 
 
 
-raw_input=raw_input("What do you want to search?: ")
+#raw_input=raw_input("What do you want to search?: ")
 def get_google_search(phrase):
   try:
     gs = GoogleSearch(phrase)
     gs.results_per_page = 50
     results = gs.get_results()
-    for res in results:
-      print res.title.encode("utf8")
-      print res.desc.encode("utf8")
-      print res.url.encode("utf8")
-      print
-  except SearchError, e:
-    print "Search failed: %s" % e
-get_google_search(str(raw_input))
+    if not results:
+      print "No results matched."
+    else:
+      for res in results:
+        print res.title.encode("utf8")
+        print res.desc.encode("utf8")
+        #print res.url.encode("utf8")
+        print
+  except SearchError:
+    print "Google search failed"
+#get_google_search(str(raw_input))
