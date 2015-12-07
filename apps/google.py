8f5ee2bf1a5b99114cd820d8b05429e79a58f21e
adamis96 <przywartyadam@gmail.com>
2015-12-07 20:59:41 +0100

    Another attempt on googling

--- apps/google.py
+++ apps/google.py
@@ -1,21 +1,32 @@
-""" Search Google, duh."""
+import json
+import requests
 
-from search import GoogleSearch, SearchError
+def get_google_search(name):
+    try:
+        answer = google_that(name)
+    except requests.exceptions.ConnectionError:
+        answer = "Sorry, couldn't connect to Google."
+    except ValueError:
+        answer = "Sorry, couldn't find anything for \"{search}\"".format(search=name)
+    return answer
 
+def google_that(thing):
+    search_results = get_results(thing)
+    str_list=[]
+    for res in search_results:
+        title = res['titleNoFormatting']
+        content = res['content']
+        str_list.append(title+content)
+    result = '\n'.join(str_list)
+    return result
 
-def search_results(phrase):
-	try:
-		gs = GoogleSearch(phrase)
-		gs.results_per_page = 30
-		results = gs.get_results()
-		if not results:
-			result = "No results matched."
-		else:
-			str_list=[]
-			for res in results:
-				str_list.append(res.title.encode("utf8"))
-				str_list.append(res.desc.encode("utf8"))
-			return '\n'.join(str_list)
-	except SearchError:
-		result = "Google search failed."
-	return result
+def get_results(phrase):
+    url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0'
+    re = requests.get( url, params={'q':phrase} )
+    json_data = json.loads(re.content.decode('utf-8'))
+    search_result = json_data['responseData']['results']
+    if search_result:
+        result=search_result
+    else:
+        raise ValueError
+    return result  
