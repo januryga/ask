296edb9c324f565fd5b636fb62d04abaf353315c
januryga <lolzatu2@gmail.com>
2015-12-09 01:20:30 +0100

    Set up packages is now safe and great.
    and took like two fucking hours to write i think? fuck batch files
    honestly

--- set_up_packages.bat
+++ set_up_packages.bat
@@ -1,4 +1,30 @@
-virtualenv --python=C:\Apps\MAMP\bin\python\bin\python lib
-lib\Scripts\activate.bat
-pip install -r requirements.txt
-deactivate
+@echo off
+
+where virtualenv
+if %ERRORLEVEL% NEQ 0 (
+	echo Couldn't find virtualenv. Please install it with pip.
+	goto :end
+) else (
+	goto :safe
+)
+
+:safe
+	echo WARNING! THIS SCRIPT WILL CLEAR THE CURRENT lib/ FOLDER!
+	set /p givenpath= "Enter a path to a Python 2.7 executable: "
+	if exist %givenpath% goto :install else goto :error
+
+
+:install
+	virtualenv --python=%givenpath% --clear lib && (
+	lib\Scripts\pip install -r requirements.txt )
+	goto :end
+
+
+:error
+	echo %givenpath% Doesn't exist or isn't a valid Python exe.
+	goto :end
+
+
+
+
+:end
